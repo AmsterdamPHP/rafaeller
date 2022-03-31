5324201aa90960bac90acf20bbff2b808f0ca813
Pauline Vos <pvos88@gmail.com>
2022-03-31 15:57:53 +0200

    Handle player messages
    So that they can join through the UI

--- src/MessageHandler.js
+++ src/MessageHandler.js
@@ -1,15 +1,29 @@
 import Player from "./classes/Player";
 
-const handleMessage = (msg, addPlayer) => {
+const handleHostMessage = (msg, addPlayer, handleError = () => console.error(msg)) => {
   const data = JSON.parse(msg)
+
+  if (data.error) {
+    handleError(data.error)
+  }
 
   switch (data.message) {
     case "newPlayer":
       addPlayer(new Player(data.username))
+      break
+    case "raffleStarted":
       break
     default:
       throw new Error('Unexpected incoming message: ' + msg)
   }
 }
 
-export default handleMessage
+const handlePlayerMessage = (msg, handleError = () => console.error(msg)) => {
+  const data = JSON.parse(msg)
+
+  if (data.error) {
+    handleError(data.error)
+  }
+}
+
+export { handleHostMessage, handlePlayerMessage }
