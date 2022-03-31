09638a0488b272d7a995299474306c044b0246d7
Pauline Vos <pvos88@gmail.com>
2022-03-31 15:56:54 +0200

    Add react router
    For a separate host view at / and a "join" view at /join

--- src/App.js
+++ src/App.js
@@ -1,70 +1,22 @@
 import "./App.css";
-import JoinCode from "./components/JoinCode"
-import Players from "./components/Players";
-import React, { useState } from 'react';
-import HostKey from "./components/HostKey";
-import Loading from "./components/Loading";
-import handleMessage from "./MessageHandler";
-
-const ws = new WebSocket('ws://localhost:8000')
+import Join from "./components/Join";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Host from "./components/Host";
 
 function App() {
-  const [hostKey, setHostKey] = useState("")
-  const [connected, setConnected] = useState(false)
-  const [joinCode, setJoinCode] = useState(false)
-  const [players, setPlayers] = useState([])
-
-  ws.onopen = () => setConnected(true)
-  const onChange = e => setHostKey(e.target.value)
-
-  const onHostKeySubmit = e => {
-    e.preventDefault()
-    const code = Math.floor(1000 + Math.random() * 9000)
-
-    ws.send(JSON.stringify({
-      "message": "registerHost",
-      "hostKey": hostKey,
-      "joinCode": code,
-    }))
-
-    setJoinCode(code)
-    setHostKey("")
-  }
-
-  const addPlayer = player => {
-    setPlayers([...players, player])
-  }
-
-  ws.onmessage = msg => handleMessage(msg.data, addPlayer)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        { connected && !joinCode
-          ? <HostKey
-            value={hostKey}
-            onSubmit={onHostKeySubmit}
-            onChange={onChange}
-          />
-          : connected
-            ? <JoinCode
-              url="http://example.org/join"
-              code={joinCode}
-          />
-            : <Loading message="waiting for raffle server..." />
-
-        }
-        <img
-          className="banner"
-          src={process.env.PUBLIC_URL + "amsterdamphp-raffler-logo.png"}
-          alt="AmsterdamPHP raffler 2.0"
-        />
-      </header>
-      {
-        connected && joinCode &&
-          <Players players={players} />
-      }
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Host />} />
+          <Route path="/join" element={<Join />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,5 @@
 export default function Avatar(props)
 {
-  console.log('hi')
   return(
       <div
         className="avatar"
