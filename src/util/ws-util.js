6122a4f5aa72388b8d73e6ee0bd2fc42132f3e2f
Pauline Vos <pvos88@gmail.com>
2022-04-20 14:45:40 +0200

    Fix keepalive function
    As it was going into an infinite loop

--- src/util/ws-util.js
+++ src/util/ws-util.js
@@ -8,9 +8,13 @@ const connect = () => {
  * Ping the WebSocket server every so often to avoid the connection closing by timeout.
  */
 const keepAlive = (ws, seconds = 25) => {
-  while (ws.readyState === WebSocket.OPEN) {
-    setTimeout(ws.send('ping'), seconds * 1000)
-  }
+  const interval = window.setInterval(() => {
+    if (ws.readyState !== ws.OPEN) {
+      window.clearInterval(interval)
+    }
+
+    ws.send('ping')
+  }, seconds * 1000 )
 }
 
 export {connect, keepAlive}
