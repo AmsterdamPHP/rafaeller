c6e30338113bab6c973ed38fd291e4ffc9658baa
Pauline Vos <pvos88@gmail.com>
2022-04-20 13:56:43 +0200

    Handle svg sanitation in Avatar component
    To deduplicate and also make sure that sanitation happens consistently
    and we don't end up having avatars that look different

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,11 +1,18 @@
 import PropTypes from "prop-types";
+import DOMPurify from "dompurify";
 
 export default function Avatar(props)
 {
+  const svg = DOMPurify.sanitize(props.svg, {
+    USE_PROFILES: { svg: true },
+  });
+
+  const className = props.className
+
   return(
       <div
-        className="avatar"
-        dangerouslySetInnerHTML={{__html: props.svg}}
+        className={`avatar ${className && className}`}
+        dangerouslySetInnerHTML={{__html: svg}}
       />
     )
 }
